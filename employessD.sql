d07080252dc3ec4ab40877c398b7e241c6d0e031
Anupamachiluveri <chiluverianupama@gmail.com>
2023-12-09 11:11:58 +0530

    database

---
+++ employessD.sql
@@ -0,0 +1,11 @@
+create database db;
+use db;
+create table employess
+(empID integer,
+empNmae varchar(20),
+empSalary double
+);
+insert into employess values(100,"anupama",10000);
+insert into employess values(200,"swathi",20000);
+insert into employess values(300,"subha",30000);
+insert into employess values(400,"swapna",40000);
